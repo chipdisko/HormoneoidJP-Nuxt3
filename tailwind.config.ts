8aff71ac5139a86c937e70bed7da83d36c723f2c
Yusuke Tan <chipdisko@gmail.com>
2023-11-04 23:10:09 +0900

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,7 +13,7 @@ module.exports = {
   theme: {
     extend: {
       animation: {
-        "text-focus-in": "text-focus-in 1s cubic-bezier(0.550, 0.085, 0.680, 0.530) both"
+        "text-focus-in": "text-focus-in .5s cubic-bezier(0.550, 0.085, 0.680, 0.530) both"
       },
       keyframes: {
           "text-focus-in": {
