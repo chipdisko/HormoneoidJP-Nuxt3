137a580186e74f99a8bd5d4283dc62366ca7d9da
Yusuke Tan <chipdisko@gmail.com>
2023-11-15 03:36:34 +0900

    fix: h2の上部が欠けてた

--- components/Onair/Page.vue
+++ components/Onair/Page.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 </script>
 <template>
-  <div class="overflow-x-hidden py-4 pb-12">
+  <div class="overflow-x-clip py-4 pb-12">
     <h2 class="title w-fit relative z-10 text-transparent text-5xl sm:text-5xl md:text-7xl lg:text-7xl xl:text-8xl 2xl:text-9xl font-tertiary skew-x-[20deg] skew-y-6 scale-y-150 lg:scale-y-100 lg:skew-y-0 scale-90 xl:translate-x-8 font-bold mb-6">Onair Archives</h2>
     <OnairList />
     <div class="copyright font-official text-6xl md:text-8xl lg:text-9xl px-2 mt-12 left-0 pl-8 ">
