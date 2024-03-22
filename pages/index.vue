318a705ee7bb97f86d18f4e144a2868b1724920d
chipdisko <chipdisko@gmail.com>
2024-03-22 13:50:15 +0900

    design: 上部にスペースを追加

--- pages/index.vue
+++ pages/index.vue
@@ -14,6 +14,6 @@ useSeoMeta({
 <template>
   <div class="lg:flex">
     <!-- <LastOnairHero class="lg:w-[45%] shrink-0 grow-0" /> -->
-    <OnairPage />
+    <OnairPage class="pt-24" />
   </div>
 </template>
