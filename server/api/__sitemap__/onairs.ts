104bed270960f6f26f77a2211655946ff5ed0038
chipdisko <chipdisko@gmail.com>
2024-07-17 17:18:57 +0900

    hasMoreで全取得

--- server/api/__sitemap__/onairs.ts
+++ server/api/__sitemap__/onairs.ts
@@ -2,12 +2,35 @@ import type { Onair as OnairProps } from "~/types/microcms";
 import type { MicroCMSListResponse } from "microcms-js-sdk";
 
 export default defineSitemapEventHandler(async () => {  
-  const response = await $fetch<MicroCMSListResponse<OnairProps>>(`/api/onairs`)
-  .then((data) => data.contents.map(p => asSitemapUrl({
+  let allContents: OnairProps[] = [];
+  let offset = 0;
+  const limit = 100;
+  let hasMore = true;
+
+  while (hasMore) {
+    const response = await $fetch<MicroCMSListResponse<OnairProps>>(`/api/onairs`, {
+      method: "GET",
+      query: {
+        limit,
+        offset,
+      },
+    }).catch((err) => {
+      console.log(err);
+      return { contents: [] };
+    });
+
+    if (response.contents.length > 0) {
+      allContents = allContents.concat(response.contents);
+      offset += limit;
+      hasMore = response.contents.length === limit;
+    } else {
+      hasMore = false;
+    }
+  }
+
+  const sitemapUrls = allContents.map(p => asSitemapUrl({
     loc: `/onair/${p.id}`,
-  }))).catch((err) => {
-    console.log(err)
-    return { contents:[] };
-  });
-  return response;
+  }));
+
+  return sitemapUrls;
 });
