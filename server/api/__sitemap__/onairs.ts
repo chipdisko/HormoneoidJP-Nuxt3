9f9b93c7ab263c2fde8b41638831800d6b396c24
chipdisko <chipdisko@gmail.com>
2024-07-17 16:52:11 +0900

    update: localのapiから取得

--- server/api/__sitemap__/onairs.ts
+++ server/api/__sitemap__/onairs.ts
@@ -1,17 +1,9 @@
 import type { Onair as OnairProps } from "~/types/microcms";
-import type { MicroCMSQueries } from "microcms-js-sdk";
-import { createClient } from "microcms-js-sdk";
+import type { MicroCMSListResponse } from "microcms-js-sdk";
 
 export default defineSitemapEventHandler(async () => {  
-  const config = useRuntimeConfig();
-  const client = createClient({
-    serviceDomain: config.microcmsServiceDomain,
-    apiKey: config.microcmsApiKey,
-  });
-
-  const response = await client.getList<OnairProps>({
-    endpoint: "onairs",
-  }).then((data) => data.contents.map(p => asSitemapUrl({
+  const response = await $fetch<MicroCMSListResponse<OnairProps>>(`/api/onairs`)
+  .then((data) => data.contents.map(p => asSitemapUrl({
     loc: `/onair/${p.id}`,
   }))).catch((err) => {
     console.log(err)
