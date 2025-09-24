f39508448cad3319f39ae7a6c38fe48a58f32a01
chipdisko <chipdisko@gmail.com>
2025-09-25 03:51:33 +0900

    fix

--- components/Image.vue
+++ components/Image.vue
@@ -14,35 +14,63 @@
   </template>
 </template>
 <script setup lang="ts">
-type Props = {
-  src?: string;
-  noImgSrc?: string;
-  width?: number;
-  height?: number;
-  quality?: number;
-  fit?: 'clip' | 'clamp' | 'crop' | 'fill' | 'scale' | 'max' | 'fillmax' | 'min';
-  crop?: 'top' | 'right top' | 'right' | 'right bottom' | 'bottom' | 'left bottom' | 'left' | 'left top' | 'center' ;
-  fill?: 'blur';
-  fm?: 'jpg' | 'png' | 'webp' | 'json';
-}
-const { src, noImgSrc, width, height, quality, fit, crop, fm } = withDefaults(defineProps<Props>(),{
-  quality: 100,
-  fit: 'max',
-  crop: 'center',
-  fill: 'blur',
-  fm: 'webp'
-})
-const defaultImageSize = {
-  width: 500,
-  height: 500,
-}
-const suffix = `?fm=${fm}&fit=${fit}${ fit === 'crop' ? `?crop=${crop.replace(' ',',')}` : `` }&q=${quality}&w=${width ?? defaultImageSize.width }&h=${height ?? defaultImageSize.height}`;
-const newSrc = src ? src.includes('images.microcms-assets.io') ? `${src}${suffix}` : src : noImgSrc ?? undefined
-
+  type Props = {
+    src?: string;
+    noImgSrc?: string;
+    width?: number;
+    height?: number;
+    quality?: number;
+    fit?:
+      | "clip"
+      | "clamp"
+      | "crop"
+      | "fill"
+      | "scale"
+      | "max"
+      | "fillmax"
+      | "min";
+    crop?:
+      | "top"
+      | "right top"
+      | "right"
+      | "right bottom"
+      | "bottom"
+      | "left bottom"
+      | "left"
+      | "left top"
+      | "center";
+    fill?: "blur";
+    fm?: "jpg" | "png" | "webp" | "json";
+  };
+  const {
+    src,
+    noImgSrc,
+    width,
+    height,
+    quality = 100,
+    fit = "max",
+    crop = "center",
+    fill = "blur",
+    fm = "webp",
+  } = defineProps<Props>();
+  const defaultImageSize = {
+    width: 500,
+    height: 500,
+  };
+  const suffix = `?fm=${fm}&fit=${fit}${
+    fit === "crop" ? `?crop=${crop.replace(" ", ",")}` : ``
+  }&q=${quality}&w=${width ?? defaultImageSize.width}&h=${
+    height ?? defaultImageSize.height
+  }`;
+  const newSrc = src
+    ? src.includes("images.microcms-assets.io")
+      ? `${src}${suffix}`
+      : src
+    : noImgSrc ?? undefined;
 </script>
 <style scoped lang="sass">
-.no-image
-  @apply w-full h-full bg-gray-200 flex items-center justify-center text-gray-400 text-lg shadow-inner
-  span
-    @apply flex items-center gap-2
+  .no-image
+    @apply w-full h-full bg-gray-200 flex items-center justify-center text-gray-400 text-lg shadow-inner
+    span
+      @apply flex items-center gap-2
 </style>
--- components/Onair/List.vue
+++ components/Onair/List.vue
@@ -1,52 +1,56 @@
 <script setup lang="ts">
-import type { MicroCMSQueries,MicroCMSListResponse } from "microcms-js-sdk";
-import type { Onair as OnairProps } from "~/types/microcms";
+  import type { MicroCMSQueries, MicroCMSListResponse } from "microcms-js-sdk";
+  import type { Onair as OnairProps } from "~/types/microcms";
 
-type Props = {
-  page?: number;
-  pagination?: boolean;
-  limit?: number | string;
-};
+  type Props = {
+    page?: number;
+    pagination?: boolean;
+    limit?: number | string;
+  };
 
-const { page, pagination, limit } = withDefaults(defineProps<Props>(), {
-  page: 1,
-  pagination: false,
-  limit: 100,
-});
+  const { page = 1, pagination = false, limit = 100 } = defineProps<Props>();
 
-const numLimit: number = Number(limit);
-const nowISOstring = ref<string>(new Date().toISOString());
-const queries: MicroCMSQueries = {
-  fields: "id,airdate,title,jacket,host,feat,description,soundcloud_embedcode",
-  limit: numLimit,
-  offset: (page - 1) * numLimit,
-  //filters: `airdate[less_than]${nowISOstring.value}`,
-};
+  const numLimit: number = Number(limit);
+  const nowISOstring = ref<string>(new Date().toISOString());
+  const queries: MicroCMSQueries = {
+    fields:
+      "id,airdate,title,jacket,host,feat,description,soundcloud_embedcode",
+    limit: numLimit,
+    offset: (page - 1) * numLimit,
+    //filters: `airdate[less_than]${nowISOstring.value}`,
+  };
 
-const { data } = await useAsyncData<MicroCMSListResponse<OnairProps>>(
-  'onairs',
-  ()=> $fetch('/api/onairs',{
-    method: 'GET',
-    query: queries
-  }),
-);
+  const { data } = await useAsyncData<MicroCMSListResponse<OnairProps>>(
+    "onairs",
+    () =>
+      $fetch("/api/onairs", {
+        method: "GET",
+        query: queries,
+      })
+  );
 
-const widths = ref<number[]>([]);
-for (let i = 0; i < Number(limit); i++) {
-  if(i === 0) {
-    widths.value.push(320);
-    continue;
+  const widths = ref<number[]>([]);
+  for (let i = 0; i < Number(limit); i++) {
+    if (i === 0) {
+      widths.value.push(320);
+      continue;
+    }
+    widths.value.push(Math.floor(Math.random() * 120) + 260);
   }
-  widths.value.push(Math.floor(Math.random() * 120) + 260);
-}
 
-//const numPages = Math.ceil((data.value?.totalCount || 0) / numLimit);
-//
+  //const numPages = Math.ceil((data.value?.totalCount || 0) / numLimit);
+  //
 </script>
 
 <template>
   <div class="flex gap-12 sm:gap-8 flex-col sm:flex-row sm:flex-wrap px-4">
-    <OnairCard v-for="article, index in data?.contents" :key="'list_'+article.id"  :article="article" class="z-10" :style="{ width: widths[index] + 'px' }" />
+    <OnairCard
+      v-for="(article, index) in data?.contents"
+      :key="'list_' + article.id"
+      :article="article"
+      class="z-10"
+      :style="{ width: widths[index] + 'px' }"
+    />
   </div>
 </template>
 ~~/types/microcms
