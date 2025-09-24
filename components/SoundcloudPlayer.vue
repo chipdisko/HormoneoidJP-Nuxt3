c5f7483324ec9ff1904983937d0be378acd81145
chipdisko <chipdisko@gmail.com>
2025-09-25 04:36:36 +0900

    fix: new embed code

--- components/SoundcloudPlayer.vue
+++ components/SoundcloudPlayer.vue
@@ -1,16 +1,14 @@
 <script setup lang="ts">
-import { storeToRefs } from 'pinia';
-import { useSoundcloud } from "~/store/soundcloud";
+  import { storeToRefs } from "pinia";
+  import { useSoundcloud } from "~/store/soundcloud";
 
-const soundcloudStore = useSoundcloud()
-const { setPlayingId, setIsOpen } = soundcloudStore;
-const { playingId, isPlaying, isOpen } = storeToRefs(soundcloudStore);
+  const soundcloudStore = useSoundcloud();
+  const { setPlayingId, setIsOpen } = soundcloudStore;
+  const { playingId, isPlaying, isOpen } = storeToRefs(soundcloudStore);
 
-const handleOpener = () => {
-  setIsOpen(!isOpen.value);
-}
-
-
+  const handleOpener = () => {
+    setIsOpen(!isOpen.value);
+  };
 </script>
 
 <template>
@@ -19,22 +17,34 @@ const handleOpener = () => {
       class="fixed z-20 bottom-0 w-full lg:w-fit left-0 transition-all duration-500 ease-in-out"
       :class="{
         ' translate-y-full': !isOpen,
-        ' translate-y-0': isOpen
-        
+        ' translate-y-0': isOpen,
       }"
     >
       <div class="relative lg:w-[560px]">
-        <button @click="handleOpener" class="absolute right-0 md:right-auto md:left-0 -top-14 text-5xl text-white hover:text-lime-400">
+        <button
+          @click="handleOpener"
+          class="absolute right-0 md:right-auto md:left-0 -top-14 text-5xl text-white hover:text-lime-400"
+        >
           <Icon
-            class=" transition-transform duration-500 ease-in-out"
+            class="transition-transform duration-500 ease-in-out"
             :class="{
               'rotate-180': isOpen,
-              'rotate-0': !isOpen
-            }" name="mdi:arrow-up-drop-circle-outline"
+              'rotate-0': !isOpen,
+            }"
+            name="mdi:arrow-up-drop-circle-outline"
           />
         </button>
-        <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" :src="`https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/${playingId}&color=%23ff5500&auto_play=${isPlaying ? 'true':'false'}&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true`"></iframe>
+        <iframe
+          width="100%"
+          height="166"
+          scrolling="no"
+          frameborder="no"
+          allow="autoplay"
+          :src="`https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/soundcloud%253Atracks%253A${playingId}&color=%23ff5500&auto_play=${
+            isPlaying ? 'true' : 'false'
+          }&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true`"
+        ></iframe>
       </div>
     </div>
   </div>
-</template>~~/store/soundcloud
+</template>
--- plugins/soundcloud.ts
+++ plugins/soundcloud.ts
@@ -2,19 +2,17 @@ export default defineNuxtPlugin(() => {
   return {
     provide: {
       extractSoundcloudIdFromEmbedcode(embedcode: string): number | undefined {
-        // 通常の形式: /tracks/123456789
-        let regex = /\/tracks\/(\d+)/;
-        let match = embedcode.match(regex);
-
-        if (match) {
-          return Number(match[1]);
+        // 新しい形式: soundcloud%253Atracks%253A数字
+        const newFormatRegex = /soundcloud%253Atracks%253A(\d+)/;
+        const newMatch = embedcode.match(newFormatRegex);
+        if (newMatch) {
+          return Number(newMatch[1]);
         }
 
-        // SoundCloud埋め込み形式: /tracks/soundcloud:tracks:123456789
-        regex = /\/tracks\/soundcloud:tracks:(\d+)/;
-        match = embedcode.match(regex);
-
-        return match ? Number(match[1]) : undefined;
+        // 古い形式: /tracks/数字（後方互換性のため）
+        const oldFormatRegex = /\/tracks\/(\d+)/;
+        const oldMatch = embedcode.match(oldFormatRegex);
+        return oldMatch ? Number(oldMatch[1]) : undefined;
       },
     },
   };
