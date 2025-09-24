efeb474c518d3c5337ce42cbbbe6670235687e56
chipdisko <chipdisko@gmail.com>
2025-09-25 04:26:36 +0900

    update: 新しいsoundcloud embed codeに対応

--- plugins/soundcloud.ts
+++ plugins/soundcloud.ts
@@ -1,12 +1,21 @@
-
 export default defineNuxtPlugin(() => {
   return {
     provide: {
-      extractSoundcloudIdFromEmbedcode(embedcode: string):(number | undefined)  {
-        const regex = /\/tracks\/(\d+)/;
-        const match = embedcode.match(regex);
+      extractSoundcloudIdFromEmbedcode(embedcode: string): number | undefined {
+        // 通常の形式: /tracks/123456789
+        let regex = /\/tracks\/(\d+)/;
+        let match = embedcode.match(regex);
+
+        if (match) {
+          return Number(match[1]);
+        }
+
+        // SoundCloud埋め込み形式: /tracks/soundcloud:tracks:123456789
+        regex = /\/tracks\/soundcloud:tracks:(\d+)/;
+        match = embedcode.match(regex);
+
         return match ? Number(match[1]) : undefined;
-      }
-    }
-  }
-})
+      },
+    },
+  };
+});
